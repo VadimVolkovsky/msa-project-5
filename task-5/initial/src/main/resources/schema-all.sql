453036041f7d8169b613fe77af1194ba22c002a2
VadimVolkovsky <v.volkovskiy@yadro.com>
2025-12-31 12:24:22 +0300

    task5 update app

--- task-5/initial/src/main/resources/schema-all.sql
+++ task-5/initial/src/main/resources/schema-all.sql
@@ -1,12 +1,14 @@
-CREATE TABLE products  (
+CREATE TABLE products (
     productId BIGINT NOT NULL PRIMARY KEY,
-    productSku BIGINT NOT NULL PRIMARY KEY,
+    productSku BIGINT NOT NULL UNIQUE,
     productName VARCHAR(20),
     productAmount BIGINT,
     productData VARCHAR(120)
 );
 
-CREATE TABLE loyality_data  (
+CREATE TABLE loyality_data (
     productSku BIGINT NOT NULL PRIMARY KEY,
-    loyalityData VARCHAR(120)
+    loyalityData VARCHAR(120),
+    CONSTRAINT fk_loyality_product
+        FOREIGN KEY (productSku) REFERENCES products(productSku)
 );
