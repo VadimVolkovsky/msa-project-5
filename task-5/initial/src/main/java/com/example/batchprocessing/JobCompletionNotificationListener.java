86098a674f5fb641e935103639a6b6fec83f0a97
VadimVolkovsky <v.volkovskiy@yadro.com>
2025-12-31 12:24:01 +0300

    task5 update app

--- task-5/initial/src/main/java/com/example/batchprocessing/JobCompletionNotificationListener.java
+++ task-5/initial/src/main/java/com/example/batchprocessing/JobCompletionNotificationListener.java
@@ -13,18 +13,37 @@ import org.springframework.stereotype.Component;
 @Component
 public class JobCompletionNotificationListener implements JobExecutionListener {
 
-	private static final Logger log = LoggerFactory.getLogger(JobCompletionNotificationListener.class);
+    private static final Logger log = LoggerFactory.getLogger(JobCompletionNotificationListener.class);
 
-	private final JdbcTemplate jdbcTemplate;
+    private final JdbcTemplate jdbcTemplate;
 
-	public JobCompletionNotificationListener(JdbcTemplate jdbcTemplate) {
-		this.jdbcTemplate = jdbcTemplate;
-	}
+    public JobCompletionNotificationListener(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
 
-	@Override
-	public void afterJob(JobExecution jobExecution) {
-		if (jobExecution.getStatus() == BatchStatus.COMPLETED) {
-			// todo
-		}
-	}
+    @Override
+    public void afterJob(JobExecution jobExecution) {
+        if (jobExecution.getStatus() == BatchStatus.COMPLETED) {
+            log.info("!!! JOB FINISHED! Waiting 30 seconds before verifying results...");
+
+            try {
+                Thread.sleep(60_000); // 60 секунд;
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+                log.warn("Sleep was interrupted", e);
+                return;
+            }
+
+            log.info("30 seconds passed. Verifying the results in the database");
+
+            jdbcTemplate
+                    .query(
+                            "SELECT productId, productSku, productName, productAmount, productData FROM products",
+                            new DataClassRowMapper<>(Product.class)
+                    )
+                    .forEach(product ->
+                            log.info("Transformed <{}> in the database.", product)
+                    );
+        }
+    }
 }
